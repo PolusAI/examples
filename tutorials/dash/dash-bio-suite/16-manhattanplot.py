8f54148a2671885ce15bc53071abb02ded4538d8
aditipate <apatel72@luc.edu>
2023-09-08 14:00:05 -0500

    dash bio examples

---
+++ tutorials/dash/dash-bio-suite/16-manhattanplot.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from dash import Dash, html, dcc, Input, Output, callback
+import dash_bio as dashbio
+from flask import Flask
+
+server = Flask(__name__)
+app = Dash(server=server)
+
+
+df = pd.read_csv('https://git.io/manhattan_data.csv')
+
+app.layout = html.Div([
+    'Threshold value',
+    dcc.Slider(
+        id='default-manhattanplot-input',
+        min=1,
+        max=10,
+        marks={
+            i: {'label': str(i)} for i in range(10)
+        },
+        value=6
+    ),
+    html.Br(),
+    html.Div(
+        dcc.Graph(
+            id='default-dashbio-manhattanplot',
+            figure=dashbio.ManhattanPlot(
+                dataframe=df
+            )
+        )
+    )
+])
+
+@callback(
+    Output('default-dashbio-manhattanplot', 'figure'),
+    Input('default-manhattanplot-input', 'value')
+)
+def update_manhattanplot(threshold):
+
+    return dashbio.ManhattanPlot(
+        dataframe=df,
+        genomewideline_value=threshold
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
